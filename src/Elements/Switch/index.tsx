19629a96c0b796a78c09e1034d4f65c1c9e16edf
Sourav Ray Chaudhuri <sourav.raychaudhuri@gmail.com>
2023-11-25 23:17:31 +0530

    Dark Mode Switch Toggle - size

--- src/Elements/Switch/index.tsx
+++ src/Elements/Switch/index.tsx
@@ -2,18 +2,18 @@ import { Switch, SwitchProps, styled } from "@mui/material";
 
 export const DarkModeSwitch = styled((props: SwitchProps) => (
     <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
-  ))(({ theme }) => ({
-    width: 56,
-    height: 30,
+  ))(({ size, theme }) => ({
+    width: size === 'small' ? 44 : 56,
+    height: size === 'small' ? 24:  30,
     padding: 0,
     margin: 8,
     '& .MuiSwitch-thumb': {
       boxSizing: 'border-box',
-      width: 26,
-      height: 26
+      width: size === 'small' ? 20 : 26,
+      height: size === 'small' ? 20 : 26
     },
     '& .MuiSwitch-track': {
-      borderRadius: 15,
+      borderRadius: size === 'small' ? 12 : 15,
       background: `linear-gradient(305deg, rgba(227,103,1,1) 0%, rgba(244,135,46,1) 61%, rgba(255,207,112,1) 100%)`,
       opacity: 1,
       transition: theme.transitions.create(['background'], {
@@ -24,15 +24,15 @@ export const DarkModeSwitch = styled((props: SwitchProps) => (
         position: 'absolute',
         top: '50%',
         transform: 'translateY(-50%)',
-        width: 22,
-        height: 22,
+        width: size === 'small' ? 16 : 22,
+        height: size === 'small' ? 16 : 22,
       },
       '&:before': {
-        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="-5 -4 26 26" width="24"><path fill="${encodeURIComponent('#FFFFFF')}" d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"/></svg>')`,
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="${size === 'small' ? '18' : '24'}" width="${size === 'small' ? '18' : '24'}" viewBox="-5 -4 26 26"><path fill="${encodeURIComponent('#FFFFFF')}" d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"/></svg>')`,
         left: 4,
       },
       '&:after': {
-        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="22" width="22" viewBox="0 0 26 26"><path fill="${encodeURIComponent('#FFFFFF')}" d="M12,7c-2.76,0-5,2.24-5,5s2.24,5,5,5s5-2.24,5-5S14.76,7,12,7L12,7z M2,13l2,0c0.55,0,1-0.45,1-1s-0.45-1-1-1l-2,0 c-0.55,0-1,0.45-1,1S1.45,13,2,13z M20,13l2,0c0.55,0,1-0.45,1-1s-0.45-1-1-1l-2,0c-0.55,0-1,0.45-1,1S19.45,13,20,13z M11,2v2 c0,0.55,0.45,1,1,1s1-0.45,1-1V2c0-0.55-0.45-1-1-1S11,1.45,11,2z M11,20v2c0,0.55,0.45,1,1,1s1-0.45,1-1v-2c0-0.55-0.45-1-1-1 C11.45,19,11,19.45,11,20z M5.99,4.58c-0.39-0.39-1.03-0.39-1.41,0c-0.39,0.39-0.39,1.03,0,1.41l1.06,1.06 c0.39,0.39,1.03,0.39,1.41,0s0.39-1.03,0-1.41L5.99,4.58z M18.36,16.95c-0.39-0.39-1.03-0.39-1.41,0c-0.39,0.39-0.39,1.03,0,1.41 l1.06,1.06c0.39,0.39,1.03,0.39,1.41,0c0.39-0.39,0.39-1.03,0-1.41L18.36,16.95z M19.42,5.99c0.39-0.39,0.39-1.03,0-1.41 c-0.39-0.39-1.03-0.39-1.41,0l-1.06,1.06c-0.39,0.39-0.39,1.03,0,1.41s1.03,0.39,1.41,0L19.42,5.99z M7.05,18.36 c0.39-0.39,0.39-1.03,0-1.41c-0.39-0.39-1.03-0.39-1.41,0l-1.06,1.06c-0.39,0.39-0.39,1.03,0,1.41s1.03,0.39,1.41,0L7.05,18.36z" /></svg>')`,
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="${size === 'small' ? '16' : '22'}" width="${size === 'small' ? '16' : '22'}" viewBox="0 0 26 26"><path fill="${encodeURIComponent('#FFFFFF')}" d="M12,7c-2.76,0-5,2.24-5,5s2.24,5,5,5s5-2.24,5-5S14.76,7,12,7L12,7z M2,13l2,0c0.55,0,1-0.45,1-1s-0.45-1-1-1l-2,0 c-0.55,0-1,0.45-1,1S1.45,13,2,13z M20,13l2,0c0.55,0,1-0.45,1-1s-0.45-1-1-1l-2,0c-0.55,0-1,0.45-1,1S19.45,13,20,13z M11,2v2 c0,0.55,0.45,1,1,1s1-0.45,1-1V2c0-0.55-0.45-1-1-1S11,1.45,11,2z M11,20v2c0,0.55,0.45,1,1,1s1-0.45,1-1v-2c0-0.55-0.45-1-1-1 C11.45,19,11,19.45,11,20z M5.99,4.58c-0.39-0.39-1.03-0.39-1.41,0c-0.39,0.39-0.39,1.03,0,1.41l1.06,1.06 c0.39,0.39,1.03,0.39,1.41,0s0.39-1.03,0-1.41L5.99,4.58z M18.36,16.95c-0.39-0.39-1.03-0.39-1.41,0c-0.39,0.39-0.39,1.03,0,1.41 l1.06,1.06c0.39,0.39,1.03,0.39,1.41,0c0.39-0.39,0.39-1.03,0-1.41L18.36,16.95z M19.42,5.99c0.39-0.39,0.39-1.03,0-1.41 c-0.39-0.39-1.03-0.39-1.41,0l-1.06,1.06c-0.39,0.39-0.39,1.03,0,1.41s1.03,0.39,1.41,0L19.42,5.99z M7.05,18.36 c0.39-0.39,0.39-1.03,0-1.41c-0.39-0.39-1.03-0.39-1.41,0l-1.06,1.06c-0.39,0.39-0.39,1.03,0,1.41s1.03,0.39,1.41,0L7.05,18.36z" /></svg>')`,
         right: 4,
       },
     },
@@ -41,7 +41,7 @@ export const DarkModeSwitch = styled((props: SwitchProps) => (
       margin: 2,
       transitionDuration: '200ms',
       '&.Mui-checked': {
-        transform: 'translateX(26px)',
+        transform: `translateX(${size === 'small' ? '20' : '26'}px)`,
         color: '#fff',
         '& + .MuiSwitch-track': {
           background: `linear-gradient(45deg, rgba(25,45,82,1) 0%, rgba(65,150,241,1) 100%)`,
